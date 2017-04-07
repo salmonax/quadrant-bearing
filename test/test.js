5feda4d68571430b7616516d0bf7cd6bda4fc083
George A. Michel <sarumonakuba@gmail.com>
2017-04-07 16:52:11 -0700

    Fix minor formatting errors in test.js

--- test/test.js
+++ test/test.js
@@ -5,12 +5,11 @@ var quadrantBearing = require('../index');
 
 
 describe('#quadrantBearing', function () {
-  it('converts azimuth to quadrant with a numeric heading',  function() {
+  it('converts azimuth to quadrant with a numeric heading', function() {
     var result = quadrantBearing(200);
     expect(result).to.equal('S20°W');
   });
   it('wraps values above 360');
   it('correctly handle negative values');
   it('correctly handles empty input');
-
 });
